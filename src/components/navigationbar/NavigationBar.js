949b405db46af364b6efd26682e9896666009a08
MacMike <92017058+CobraKarl@users.noreply.github.com>
2022-01-04 17:14:24 +0100

    added som more files

--- src/components/navigationbar/NavigationBar.js
+++ src/components/navigationbar/NavigationBar.js
@@ -1,11 +1,18 @@
 import React from "react"
 import "./NavigationBar.css"
+import Logotype from "../../shared/images/logotype.svg"
+import {useNavigate} from "react-router-dom"
 
 export const Navigationbar = () => {
+    const navigate = useNavigate();
+
     return (
         <div className="navigationBarWrapper">
-             <span>Home</span>
-             <span>Sign in</span>
+             <img onClick={() => navigate ("/")}
+             className="logotype"
+             src={Logotype} alt="Error..."/>
+             <span onClick={() => navigate ("/signin")}
+              className="signIn">Sign in</span>
         </div>
     )
 }
