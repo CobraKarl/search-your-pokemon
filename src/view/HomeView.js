892123ff26242e35387e2649f2bee2422fcb26b6
MacMike <92017058+CobraKarl@users.noreply.github.com>
2022-01-10 15:05:20 +0100

    Updated with part 16

--- src/view/HomeView.js
+++ src/view/HomeView.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 export const HomeView = () => {
+    const [count, setcount] = useState(0)
+
+    useEffect(() => {
+        alert("Component is being rendered")
+        return () => {
+            alert("Componment is being removed")
+        }
+    }, [count])
+
     return(
         <div>
-            <h1>This is the HomeView page</h1>
+            <h1>{count}</h1>
+            <button onClick={() => setcount(count + 1)}>Increment with 1</button>
         </div>
     )
 }
