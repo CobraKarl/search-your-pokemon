7408ec65b45657561bde3886351beede8b5d29c9
MacMike <92017058+CobraKarl@users.noreply.github.com>
2022-01-13 14:53:27 +0100

    uppdated with part 21

--- src/view/HomeView.js
+++ src/view/HomeView.js
@@ -1,9 +1,17 @@
 import React from "react";
+import Axios from "axios"
 
 export const HomeView = () => {
+
+    const fetchDataFromExternalAPI = () => {
+        Axios.get("https://pokeapi.co/api/v2/pokemon/mewtwo")
+        .then((Response) => console.log(Response))
+        .catch((Error) => console.log(Error))
+    }
+
     return(
         <div>
-            <h1>This is the HomeView!!</h1>
+            <button onClick={() => fetchDataFromExternalAPI()}>Make API call</button>
         </div>
     )
 }
